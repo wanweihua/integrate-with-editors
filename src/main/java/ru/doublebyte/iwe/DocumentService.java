7e7594b70d6caea0fd61c0dba6298f488eb8e02a
Alexey Nurgaliev <atnurgaliev@gmail.com>
2016-07-19 13:13:15 +0300

    list and delete

--- src/main/java/ru/doublebyte/iwe/DocumentService.java
+++ src/main/java/ru/doublebyte/iwe/DocumentService.java
@@ -1,5 +1,6 @@
 package ru.doublebyte.iwe;
 
+import com.google.common.collect.Lists;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +14,9 @@ import ru.doublebyte.iwe.types.DocumentType;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -62,6 +66,40 @@ public class DocumentService {
     }
 
     /**
+     * Delete document
+     * @param id Document id
+     */
+    public void delete(Long id) throws Exception {
+        logger.info("Delete document with id {}", id);
+        try {
+            Document document = documentRepository.findOne(id);
+            if(document == null) {
+                throw new Exception("Document not found: " + id);
+            }
+
+            documentRepository.delete(id);
+            deleteFile(document.getStorageId());
+        } catch(Exception e) {
+            logger.error("Document delete error", e);
+            throw new Exception("Document delete error", e);
+        }
+    }
+
+    /**
+     * Get all documents
+     * @return Documents
+     */
+    public List<Document> list() {
+        List<Document> documents = Lists.newArrayList(documentRepository.findAll());
+
+        Collections.sort(documents, (a, b) -> a.getName().compareTo(b.getName()));
+
+        return documents;
+    }
+
+    ///////////////////////////////////////////////////////////////////////////
+
+    /**
      * Check whether storage path exists
      */
     protected void ensureStoragePathExists() {
@@ -77,23 +115,37 @@ public class DocumentService {
     }
 
     /**
-     * Save document in storage
-     * @param storageId Id in storage
-     * @param data Document data
+     * Get document path by storage id
+     * @param storageId Storage id
+     * @return Path
+     * @throws Exception When storage id is broken
      */
-    protected void save(String storageId, byte[] data) throws Exception {
+    protected Path getDocumentPath(String storageId) throws Exception {
         ensureStoragePathExists();
 
         Path storage = Paths.get(storagePath).toAbsolutePath();
         Path documentPath = Paths.get(storagePath, storageId).toAbsolutePath();
 
         if(!documentPath.startsWith(storage)) {
-            throw new Exception("Access violation while saving file with storage id " + storageId);
+            throw new Exception("Access violation with storage id " + storageId);
         }
 
-        Files.write(documentPath, data);
+        return documentPath;
+    }
 
+    /**
+     * Save document in storage
+     * @param storageId Id in storage
+     * @param data Document data
+     */
+    protected void save(String storageId, byte[] data) throws Exception {
+        Files.write(getDocumentPath(storageId), data);
         logger.info("Saved document with storage id {}", storageId);
+    }
+
+    protected void deleteFile(String storageId) throws Exception {
+        Files.delete(getDocumentPath(storageId));
+        logger.info("Removed document with storage id {}", storageId);
     }
 
     ///////////////////////////////////////////////////////////////////////////
--- src/main/java/ru/doublebyte/iwe/IndexController.java
+++ src/main/java/ru/doublebyte/iwe/IndexController.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -24,7 +25,8 @@ public class IndexController {
     }
 
     @RequestMapping("/")
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("documents", documentService.list());
         return "index";
     }
 
@@ -39,6 +41,21 @@ public class IndexController {
         } catch(Exception e) {
             logger.error("File upload error", e);
             redirectAttributes.addFlashAttribute("upload_message", e.getMessage());
+        }
+        return "redirect:/";
+    }
+
+    @RequestMapping(path = "/delete", method = RequestMethod.POST)
+    public String delete(
+        @RequestParam("id") Long id,
+        RedirectAttributes redirectAttributes
+    ) {
+        try {
+            documentService.delete(id);
+            redirectAttributes.addFlashAttribute("delete_message", "Document deleted successfully");
+        } catch(Exception e) {
+            logger.error("Document delete error", e);
+            redirectAttributes.addFlashAttribute("delete_message", e.getMessage());
         }
         return "redirect:/";
     }
